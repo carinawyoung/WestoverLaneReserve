902b08883dc942067b764fd3d814d757acafa9fa
carinawyoung <84910539+carinawyoung@users.noreply.github.com>
2024-07-08 10:57:53 -0400

    Changed schedule column headers to be the Days and tades of the current week

--- WestoverLaneReserve/Pages/LaneSchedule.cshtml.cs
+++ WestoverLaneReserve/Pages/LaneSchedule.cshtml.cs
@@ -1,10 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
 
 namespace WestoverLaneReserve.Pages
 {
     public class LaneScheduleModel : PageModel
     {
+        public List<string> WeekDates { get; private set; } = new List<string>();
 
+        public void OnGet()
+        {
+            WeekDates = GetCurrentWeekDates();
+        }
+
+        private List<string> GetCurrentWeekDates()
+        {
+            List<string> weekdates = new List<string>();
+            DateTime today = DateTime.Today;
+            int daysUntilSunday = (int)DayOfWeek.Sunday - (int)today.DayOfWeek;
+            DateTime startOfWeek = today.AddDays(daysUntilSunday);
+
+            for (int i = 0; i < 7; i++)
+            {
+                DateTime date = startOfWeek.AddDays(i);
+                weekdates.Add(date.ToString("ddd M/d", CultureInfo.InvariantCulture));
+            }
+
+            return weekdates;
+        }
     }
 }
