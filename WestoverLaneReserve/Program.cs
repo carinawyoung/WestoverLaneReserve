1fd22f3502f4af0b42f232d17773c7049a58fc95
carinawyoung <84910539+carinawyoung@users.noreply.github.com>
2024-07-09 09:30:35 -0400

    forgot to save Program.cs

--- WestoverLaneReserve/Program.cs
+++ WestoverLaneReserve/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WestoverLaneReserve.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Add DbContext using SQLite
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -21,5 +28,13 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+// Make sure database is created and migrations are applied:
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
 
 app.Run();
