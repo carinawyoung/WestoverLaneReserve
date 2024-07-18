096347bf2f609cb0e93f105229c30c866558a01b
carinawyoung <84910539+carinawyoung@users.noreply.github.com>
2024-07-18 15:01:36 -0400

    finished with Part C of Task 3

--- WestoverLaneReserve/obj/Debug/net8.0/WestoverLaneReserve.AssemblyInfo.cs
+++ WestoverLaneReserve/obj/Debug/net8.0/WestoverLaneReserve.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WestoverLaneReserve")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8eb953da1bb012224748b6862c7c6b85b2d0529")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3402fc6d7eb992ee45aef4179f2662ee0b3c7fee")]
 [assembly: System.Reflection.AssemblyProductAttribute("WestoverLaneReserve")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WestoverLaneReserve")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
